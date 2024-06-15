352aa705c90d08a7013d31d36dae58fb855cbae3
Yem Daro <100412991+darororo@users.noreply.github.com>
2024-06-16 01:07:02 +0700

    testing window popup feature

---
+++ view/popup/sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+function draw() {
+    background(200);
+    text('hi', 50, 50);
+}
